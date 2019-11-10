bc3e7856937193a85aae9f1fa772ee7e3e765c14
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-11-10 22:13:38 +0900

    Add AccessToken

---
+++ class/entity/AccessToken.php
@@ -0,0 +1,89 @@
+<?php
+
+class AccessToken extends AbstractEntity
+{
+    /** @var int */
+    public $oauth2_client_id;
+    /** @var int */
+    public $customer_id;
+    /** @var string */
+    public $access_token;
+    /** @var string */
+    public $refresh_token;
+    /** @var string */
+    public $token_type;
+    /** @var string */
+    public $id_token;
+    /** @var int */
+    public $expires_in;
+    /**
+     * @var DateTime
+     */
+    private $create_date;
+    /**
+     * @var DateTime
+     */
+    private $update_date;
+    /** @var string */
+    public $scope;
+
+    public function __construct(array $properties = [])
+    {
+        self::setPropertiesFromArray($properties);
+        if (!$this->create_date instanceof DateTime && strtotime($this->create_date) !== false) {
+            $this->setCreateDate(new DateTime($this->create_date));
+        }
+        if (!$this->update_date instanceof DateTime && strtotime($this->update_date) !== false) {
+            $this->setUpdateDate(new DateTime($this->update_date));
+        }
+    }
+
+    public function setCreateDate(DateTime $create_date)
+    {
+        $this->create_date = $create_date;
+
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreateDate()
+    {
+        return $this->create_date;
+    }
+
+    public function setUpdateDate(DateTime $update_date)
+    {
+        $this->update_date = $update_date;
+
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getUpdateDate()
+    {
+        return $this->update_date;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getExpire()
+    {
+        if (!$this->getCreateDate() instanceof DateTime) {
+            return new DateTime(date('Y-m-d H:i:s', 0));
+        }
+        return $this->getCreateDate()->add(new DateInterval('PT'.$this->expires_in.'S'));
+    }
+
+    /**
+     * @return bool
+     */
+    public function isExpired()
+    {
+        return (new DateTime() > $this->getExpire());
+    }
+}
---
+++ tests/class/entity/AccessTokenTest.php
@@ -0,0 +1,73 @@
+<?php
+
+class AccessTokenTest extends Common_TestCase
+{
+    /** @var array */
+    protected $token = [];
+    /** @var Faker\Generator */
+    private $faker;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->faker = Faker\Factory::create('ja_JP');
+        $this->token = [
+            'oauth2_client_id' => $this->faker->randomNumber(),
+            'customer_id' => $this->faker->randomNumber(),
+            'access_token' => $this->faker->uuid,
+            'refresh_token' => $this->faker->uuid,
+            'token_type' => 'Bearer',
+            'expires_in' => 3600,
+            'create_date' => date('Y-m-d H:i:s'),
+            'update_date' => date('Y-m-d H:i:s'),
+            'scope' => 'profile openid'
+        ];
+    }
+
+    public function testCreateInstance()
+    {
+        $objToken = new AccessToken($this->token);
+
+        $this->assertEquals($this->token['create_date'], $objToken->getCreateDate()->format('Y-m-d H:i:s'));
+    }
+
+    public function testGetExpire()
+    {
+        $this->token['expires_in'] = 100;
+        $now = new DateTime();
+        $this->token['create_date'] = $now->format('Y-m-d H:i:s');
+        $expected = $now->add(new DateInterval('PT100S'));
+        $objToken = new AccessToken($this->token);
+
+        $actual = $objToken->getExpire();
+        $this->assertEquals($expected->format('Y-m-d H:i:s'), $actual->format('Y-m-d H:i:s'));
+    }
+
+    public function testGetExpireWithNull()
+    {
+        $this->token['create_date'] = null;
+        $this->token['update_date'] = null;
+        $objToken = new AccessToken($this->token);
+
+        $actual = $objToken->getExpire();
+        $this->assertNull($objToken->getCreateDate());
+        $this->assertNull($objToken->getUpdateDate());
+        $this->assertEquals('0', $actual->format('U'));
+    }
+
+    public function testIsExpired()
+    {
+        $this->token['expires_in'] = 10;
+        $this->token['create_date'] = new DateTime('-1 minutes');
+        $objToken = new AccessToken($this->token);
+        $this->assertTrue($objToken->isExpired());
+    }
+
+    public function testIsNotExpired()
+    {
+        $this->token['expires_in'] = 10;
+        $this->token['create_date'] = new DateTime();
+        $objToken = new AccessToken($this->token);
+        $this->assertFalse($objToken->isExpired());
+    }
+}
